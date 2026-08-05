28aa1ceba2e679d0c45d98e7d07ccc0bc8a28d59
Qing <byeomax@outlook.com>
2026-08-05 17:04:11 -0400

    chore(version): bump version to 1.3.1

--- js/config/version.js
+++ js/config/version.js
@@ -4,5 +4,5 @@
  */
 (function setAppVersion(global) {
     if (!global) return;
-    global.APP_VERSION = 'v1.3.0';
+    global.APP_VERSION = 'v1.3.1';
 })(typeof window !== 'undefined' ? window : null);
